da45d30398a200f759bc5b8a03954e031dcb38ae
OlegApanovich <dollar4444@gmail.com>
2025-05-22 12:12:49 +0300

    #

---
+++ src/WooCommerce/Sniffs/Escaping/EscapingDetection.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Sniff to improve escaping detection.
+ */
+
+namespace WooCommerce\Sniffs\Escaping;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+/**
+ * Comment tags sniff.
+ */
+class EscapingDetection implements Sniff
+{
+
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = [
+        'PHP',
+        'JS',
+    ];
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register(): array
+    {
+        return [T_ECHO];
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
+     * @param int                         $stackPtr  The position of the current token
+     *                                               in the stack passed in $tokens.
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $nextToken = $phpcsFile->findNext(T_WHITESPACE, $stackPtr + 1, null, true);
+
+        if ($nextToken === false || $tokens[$nextToken]['code'] !== T_VARIABLE) {
+            return;
+        }
+
+        $phpcsFile->addError(
+            'Outputting a variable with echo without escaping.',
+            $nextToken,
+            'UnescapedEcho'
+        );
+    }
+}
